867e9e3e65f739f13f2ed0672ee8ef2a2a98d25f
tarbow-M <2020shikata@gmail.com>
2025-11-04 14:34:24 +0900

    親プロの削除機能
    what
    ・turboオフによりjavascriptの競合エラーが起きていたのでconfirm行削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,19 @@
 // Entry point for the esbuild build script in package.json
 // import "@hotwired/turbo-rails"
-import * as ActiveStorage from "@rails/activestorage"
+// import * as ActiveStorage from "@rails/activestorage"
 
-// ActiveStorage を開始
-ActiveStorage.start()
+// // ActiveStorage を開始
+// ActiveStorage.start()
+
+// Rails UJS (data-method, data-confirm などのため)
+import Rails from "@rails/ujs"
+
+// Active Storage (ダイレクトアップロードのため)
+import * as ActiveStorage from "@rails/activestorage"
 
 // (Stimulus コントローラは /controllers フォルダが作成されたらここに追加します)
 // import "./controllers"
+
+// Rails UJS と Active Storage を開始
+Rails.start()
+ActiveStorage.start()
