638192d030c5b8c77afb0942b26e2a1fad98839b
tarbow-M <2020shikata@gmail.com>
2025-11-12 10:58:33 +0900

    PostgreSQL#2

--- app/models/template.rb
+++ app/models/template.rb
@@ -6,7 +6,11 @@ class Template < ApplicationRecord
   # --- PostgreSQLのJsonb型に対応するためのメソッド ---
   # rangeがnilの場合に空のハッシュを返す
   def range
-    read_attribute(:range) || {}
+    data = read_attribute(:range) || {}
+    # range.a や range.b が nil になることを防ぐ
+    data['a'] ||= []
+    data['b'] ||= []
+    data
   end
 
   # mappingがnilの場合に空のハッシュを返す
